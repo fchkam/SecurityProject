cb20989eb6f196e32136cfa49ff43c235d7531c8
Kent Ma <m.kentma@gmail.com>
2014-07-29 22:33:46 -0400

    Updated to AES/CBC/PKCS5Padding

--- ImageProcessing/app/src/main/java/image/processing/app/CryptoManager.java
+++ ImageProcessing/app/src/main/java/image/processing/app/CryptoManager.java
@@ -8,14 +8,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.spec.AlgorithmParameterSpec;
 
 import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Created by kentma on 2014-07-29.
@@ -28,7 +31,7 @@ public class CryptoManager {
     Cipher decryptCipher;
 
     private CryptoManager() {
-        initCiphers("password".toCharArray());
+        initCiphers("e8ffc7e56311679f12b6fc91aa77a5eb".getBytes());
     }
 
     public static CryptoManager getInstance() {
@@ -62,7 +65,7 @@ public class CryptoManager {
     }
 
 
-    public void initCiphers(char password[]) {
+    public void initCiphers(byte[] keybytes) {
 
         PBEKeySpec pbeKeySpec;
         PBEParameterSpec pbeParamSpec;
@@ -70,18 +73,18 @@ public class CryptoManager {
 
         byte[] salt = {
                 (byte) 0xc7, (byte) 0x73, (byte) 0x21, (byte) 0x8c,
+                (byte) 0x7e, (byte) 0xc8, (byte) 0xee, (byte) 0x99,
+                (byte) 0xc7, (byte) 0x73, (byte) 0x21, (byte) 0x8c,
                 (byte) 0x7e, (byte) 0xc8, (byte) 0xee, (byte) 0x99
         };
-        int count = 20;
-        pbeParamSpec = new PBEParameterSpec(salt, count);
-        pbeKeySpec = new PBEKeySpec(password);
+
+        IvParameterSpec ivSpec = new IvParameterSpec(salt);
+        SecretKeySpec newKey = new SecretKeySpec(keybytes, "AES");
         try {
-            keyFac = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
-            SecretKey pbeKey = keyFac.generateSecret(pbeKeySpec);
-            encryptCipher = Cipher.getInstance("PBEWithMD5AndDES");
-            decryptCipher = Cipher.getInstance("PBEWithMD5AndDES");
-            encryptCipher.init(Cipher.ENCRYPT_MODE, pbeKey, pbeParamSpec);
-            decryptCipher.init(Cipher.DECRYPT_MODE, pbeKey, pbeParamSpec);
+            encryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            decryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+            decryptCipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec);
         } catch (Exception e) {
             Log.v("tag", e.toString());
         }
